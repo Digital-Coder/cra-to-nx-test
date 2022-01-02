db4a40d83292f9ebf649e56c9e4ac7979f7be590
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-01-02 13:34:51 +0100

    test affected:apps

--- apps/cra-to-nx-test/src/App.js
+++ apps/cra-to-nx-test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          nedit <code>src/App.js</code> and save to reload.
+         cra-to-nx-test
         </p>
         <a
           className="App-link"
