71d94201a42bdb6327a6dec7046e92f9b495fd3c
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-01-07 15:50:33 +0100

    Revert "generate shared component"
    This reverts commit 8d59f73f43e11bfd073c481bdabbb685f5b8f94b.

--- libs/ui/src/lib/todos/todos.js
+++
@@ -1,9 +0,0 @@
-import "./todos.module.css";
-export function Todos(props) {
-  return (
-    <div>
-      <h1>Welcome to Todos!</h1>
-    </div>
-  );
-}
-export default Todos;
--- libs/ui/src/lib/ui.js
+++
@@ -1,9 +0,0 @@
-import "./ui.module.css";
-export function Ui(props) {
-  return (
-    <div>
-      <h1>Welcome to Ui!</h1>
-    </div>
-  );
-}
-export default Ui;
