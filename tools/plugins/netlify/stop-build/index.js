f2ac847f05252a480c241e2dbd282e77723b0bc6
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-01-13 19:30:59 +0100

    fix build-stop

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -24,10 +24,10 @@ function projectChanged(projectName, lastDeployedCommit) {
   const filteredArray = npmOutput.split(/\r\n|\r|\n/);
   console.log(filteredArray, "filtered   npm output array");
   //convert project names to array
-  const affectedProjects = filteredArray[4].split(" ");
+  //const affectedProjects = filteredArray[4].split(" ");
   //if netlify app name is the same as affected app name, return true
   const foundChangedProject =
-    affectedProjects.findIndex((project) => project === projectName) > -1;
+    filteredArray.findIndex((project) => project === projectName) > -1;
 
   console.log("Found git changes in these projects: " + affectedProjects);
 }
