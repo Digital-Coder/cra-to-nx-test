269c65c6495f7e77f9cd411bebd92e06d5e833a9
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-01-13 19:47:21 +0100

    try to deploy only monitoring app

--- apps/senf-monitoring/src/App.js
+++ apps/senf-monitoring/src/App.js
@@ -41,7 +41,7 @@ import detectLocation from "./util/detectLocation";
 
 import "./util/i18n";
 detectLocation(); // detect location and set i18n language
-console.log("deploy only monitor");
+
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
   firebase
