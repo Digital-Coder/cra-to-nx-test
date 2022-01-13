059f2cdc1e1bbb789b0a81330bc1862e024fde47
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-01-13 14:18:41 +0100

    add netlify stop-build updated version

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,32 +1,33 @@
+const { basename } = require("path");
+
 module.exports = {
-  onInit: ({ utils }) => {
-    const currentProject = process.env.PROJECT_NAME;
+  onPreBuild: ({ utils, constants }) => {
+    const projectName =
+      process.env.PROJECT_NAME || basename(constants.PUBLISH_DIR);
     const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
     const latestCommit = "HEAD";
     const projectHasChanged = projectChanged(
-      currentProject,
+      projectName,
       lastDeployedCommit,
       latestCommit
     );
     if (!projectHasChanged) {
       utils.build.cancelBuild(
-        `Build was cancelled because ${currentProject} was not affected by the latest changes`
+        `Build was cancelled because ${projectName} was not affected by the latest changes`
       );
     }
   },
 };
-
+// eslint-disable-next-line no-unused-vars
 function projectChanged(currentProject, fromHash, toHash) {
   const execSync = require("child_process").execSync;
-  const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+  const getAffected = `nx print-affected  --plain --base=${fromHash} --head=${toHash}`;
   const output = execSync(getAffected).toString();
   //get the list of changed projects from the output
-  const changedProjects = JSON.parse(output).projects;
-  if (changedProjects.find((project) => project === currentProject)) {
-    return true;
-  } else {
-    return false;
-  }
+  const changedProjects = JSON.parse(output).projects; // array of affected projects
+  return (
+    changedProjects.findIndex((project) => project === currentProject) > -1
+  );
 }
 
 /* function projectChanged(){
