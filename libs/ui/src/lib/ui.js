13e40fe3c5cdbfb825da8ef7c5391575ff5425b9
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-01-18 16:32:06 +0100

    add submit button to lib

--- libs/ui/src/lib/ui.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-const StyledUi = styled.div`
-  color: pink;
-`;
-export function Button() {
-  return <StyledUi>button</StyledUi>;
-}
-export default Button;
